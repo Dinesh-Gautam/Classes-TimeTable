f337504c2939cb05e87aed7abf2394541b851a10
Dinesh-Gautam <dineshgautam5252@gmail.com>
2021-05-21 22:25:40 +0530

    initiallizing code for general notes

---
+++ scripts_v3/data.js
@@ -0,0 +1,167 @@
+let timeTable = {
+    1: [
+      {
+        startTime: 9,
+        endTime: 9.48,
+        subject: "Physics",
+      },
+      {
+        startTime: 9.48,
+        endTime: 10.38,
+        subject: "SCA",
+      },
+      {
+        startTime: 10.38,
+        endTime: 11.28,
+        subject: "ECS",
+      },
+      {
+        startTime: 11.28,
+        endTime: 12.18,
+        subject: "BE",
+      },
+      {
+        startTime: 12.18,
+        endTime: 14.18,
+        subject: "Math",
+      },
+      {
+        startTime: 14.18,
+        endTime: 17,
+        subject: "Computer ws",
+      },
+    ],
+    2: [
+      {
+        startTime: 9,
+        endTime: 9.48,
+        subject: "Physics",
+      },
+      {
+        startTime: 9.48,
+        endTime: 10.38,
+        subject: "SCA",
+      },
+      {
+        startTime: 10.38,
+        endTime: 11.28,
+        subject: "Math",
+      },
+      {
+        startTime: 11.28,
+        endTime: 12.18,
+        subject: "ECS",
+      },
+      {
+        startTime: 12.18,
+        endTime: 14.18,
+        subject: "EVS",
+      },
+      {
+        startTime: 14.18,
+        endTime: 15.08,
+        subject: "BE",
+      },
+      {
+        startTime: 15.08,
+        endTime: 17,
+        subject: "BE",
+      },
+    ],
+    3: [
+      {
+        startTime: 9,
+        endTime: 11.28,
+        subject: "Computer ws",
+      },
+      {
+        startTime: 11.28,
+        endTime: 14.18,
+        subject: "DTP",
+      },
+      {
+        startTime: 14.18,
+        endTime: 15.08,
+        subject: "Math",
+      },
+      {
+        startTime: 15.08,
+        endTime: 15.58,
+        subject: "Physics",
+      },
+      {
+        startTime: 15.58,
+        endTime: 17,
+        subject: "SCA",
+      },
+    ],
+    4: [
+      {
+        startTime: 9,
+        endTime: 9.48,
+        subject: "Math",
+      },
+      {
+        startTime: 9.48,
+        endTime: 10.38,
+        subject: "EVS",
+      },
+      {
+        startTime: 10.38,
+        endTime: 11.28,
+        subject: "SCA",
+      },
+      {
+        startTime: 11.28,
+        endTime: 13.18,
+        subject: "BE",
+      },
+      {
+        startTime: 13.18,
+        endTime: 15.08,
+        subject: "DTP",
+      },
+      {
+        startTime: 15.08,
+        endTime: 17,
+        subject: "physics",
+      },
+    ],
+    5: [
+      {
+        startTime: 9,
+        endTime: 9.48,
+        subject: "Math",
+      },
+      {
+        startTime: 9.48,
+        endTime: 10.38,
+        subject: "Physics",
+      },
+      {
+        startTime: 10.38,
+        endTime: 12.18,
+        subject: "ECS",
+      },
+      {
+        startTime: 12.18,
+        endTime: 14.18,
+        subject: "SCA",
+      },
+      {
+        startTime: 14.18,
+        endTime: 15.08,
+        subject: "ECS",
+      },
+      {
+        startTime: 15.08,
+        endTime: 15.58,
+        subject: "BE",
+      },
+      {
+        startTime: 15.58,
+        endTime: 17,
+        subject: "EVS",
+      },
+    ],
+  };
